4ccbe7778178dfd8cdb30ec6d8e051ae584a0fd8
Mateusz <kontakt@mateuszdomagala.pl>
2021-07-07 20:20:58 +0200

    substitute negative test for positive one

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,4 @@
-let isPalindrome = (text) => word === word.split('').reverse('').join('');
+let isPalindrome = (phrase) => phrase === phrase.split('').reverse('').join('');
 
 
 describe('the palindrome canary spec', () => {
@@ -11,8 +11,9 @@ describe('the palindrome canary spec', () => {
   it('false for dude', () => {
     expect(isPalindrome('dude')).toBe(false);
   });
-  it.todo ('true for mom mom');
-  it.todo ('false for dad mom');
+  it('false for dad mom', () => {
+    expect(isPalindrome('dad mom')).toBe(false);
+  });
   it.todo ('true for whitespace');
   it.todo ('error for empty string');
   it.todo ('error for not a string');
