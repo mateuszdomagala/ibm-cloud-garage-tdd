556b501c5e83d35a6d5ca2863fd6c83093ec9420
Mateusz <kontakt@mateuszdomagala.pl>
2021-07-07 19:59:58 +0200

    make bar green for first test

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,8 +1,15 @@
+let isPalindrome = () => {
+  return true;
+}
+
+
 describe('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
-  it.todo ('true for mom');
+  it('true for mom', () => {
+    expect(isPalindrome('mom')).toBe(true);
+  });
   it.todo ('false for dude');
   it.todo ('true for mom mom');
   it.todo ('false for dad mom');
